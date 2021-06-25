38040b0790d9bc74ca3945d82f3cdad9ecdf5318
sahilinux <83116166+sahilinux@users.noreply.github.com>
2021-06-25 14:28:46 +0530

    Add files via upload

---
+++ 01_dictionary_syntax.py
@@ -0,0 +1,13 @@
+myDict = { 
+    "fast": "In a Quick Manner",
+    "Harry": "A Coder",
+    "Marks": [1,3,3],
+    "anotherdict": {'harry': 'player'}
+
+}
+# print(myDict['fast'])
+# print(myDict['Harry'])
+myDict['Marks'] = [45 ,55]
+print(myDict["Marks"])
+print(myDict["anotherdict"]['harry'])
+
---
+++ 02_dictionary_methods.py
@@ -0,0 +1,23 @@
+myDict = { 
+    "fast": "In a Quick Manner",
+    "Harry": "A Coder",
+    "Marks": [1,3,3],
+    "anotherdict": {'harry': 'player'},
+    1: 2
+}
+# Dictionary Methods
+print(myDict.keys()) # Prints the keys of the dictionary
+print(list(myDict.keys())) # Prints the keys of dictionary
+print(myDict.items())
+print(myDict.values()) # Prints the (key, value) for all contents of Dictionary
+print(myDict)
+updateDict = {
+
+    "Lovish": "friend",
+    "Raka": "Classmate"
+}
+myDict.update(updateDict)
+print(myDict)
+
+
+
