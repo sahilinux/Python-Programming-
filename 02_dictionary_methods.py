4d6741caf818257da178a5b97015da398929793e
sahilinux <83116166+sahilinux@users.noreply.github.com>
2021-06-26 13:51:32 +0530

    Add files via upload

--- 02_dictionary_methods.py
+++ 02_dictionary_methods.py
@@ -13,11 +13,26 @@ print(myDict.values()) # Prints the (key, value) for all contents of Dictionary
 print(myDict)
 updateDict = {
 
-    "Lovish": "friend",
-    "Raka": "Classmate"
+    "Lovish": "Friend",
+    "Raka": "Classmate",
+    "Rajveer": "Friend",
+    "Harry": "A Rapper" # this will update ("Harry": "A Coder") to ("Harry": "A Rapper")
 }
+myDict.update(updateDict) # Updates the dictionary by aadding key-value pairs from updateDict
+# print(myDict)
+
+print(myDict.get("Harry2")) # Prints value associated with key "harry"
+print(myDict["Harry2"]) # Prints value associated with key "harry"
+
+# The diffrence between .get and [] syntax in Dictionaries?
+print(myDict.get("harry2")) # returns None as harry2 is not present in the dictionary
+print(myDict["harry2"]) # throws an error as harry2 is not present in the dictionary
+
+
+updateDict = {
+    "Harry": "A Programmer" # update ("A Rapper") to ("A Programmmer")
+}
+
 myDict.update(updateDict)
 print(myDict)
-
-
 
