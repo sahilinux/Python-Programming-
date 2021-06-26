f5e72aba19cac969e7edb1521bbca25d9b51901e
sahilinux <83116166+sahilinux@users.noreply.github.com>
2021-06-26 13:52:41 +0530

    Add files via upload

---
+++ 04_empty_set.py
@@ -0,0 +1,7 @@
+# Important: This syntax will creat an emty dictionary and not an ampty set
+a = {}
+print(type(a))
+
+# An empty set can be created using the below Syntax:
+b = set()
+print(type(b))
